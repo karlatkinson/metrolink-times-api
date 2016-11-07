6a91f70b02d5b93d688267e1586d99b245d31efd
Karl Atkinson <karl.atkinson@gmail.com>
2016-11-07 09:46:03 +0000

    refactoring

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const errorHandler = require('./lib/errorHandler');
 const app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(req, res) {
-    return res.json({
-        status: 'nothing to see here....'
-    });
-});
+const port = process.env.PORT || 5000;
 
 app.use('/', require('./lib/status'));
 app.use('/api/stations', require('./lib/stations'));
 app.use(errorHandler);
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+app.listen(port, () => {
+    console.log('Node app is running on port', port);
 });
 
 exports = module.exports = app;

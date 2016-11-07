5f2f12b7baac3ac19cacbcf574d7449bceb7cdea
Karl Atkinson <karl.atkinson@gmail.com>
2016-11-07 09:26:22 +0000

    error handling and cleaned up structure

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
 var express = require('express');
-//var fs = require('fs');
-var rp = require('request-promise');
-var cheerio = require('cheerio');
+var errorHandler = require('./lib/errorHandler');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, res) {
-    return res.json({ status: 'nothing to see here....'});
+    return res.json({
+        status: 'nothing to see here....'
+    });
 });
 
-app.get('/api/stations/:station', function(req, res) {
-
-    var options = {
-        uri: `https://beta.tfgm.com/public-transport/stations/${req.params.station}`,
-        transform: function(body) {
-            return cheerio.load(body);
-        }
-    };
-
-    var times = [];
-
-    rp(options)
-        .then(function($) {
-            $('#departure-items tr').each(function() {
-                var destination = $(this).find('.departure-destination').text();
-                var type = $(this).find('.departure-carriages span').first().text();
-                var wait = $(this).find('.departure-wait').find('.figure').text();
-                var unit = $(this).find('.departure-wait').find('.unit').text();
-
-                times.push({
-                    destination,
-                    type,
-                    due: `${wait} ${unit}`
-                })
-            });
-
-            return res.json(times);
-        })
-        .catch(function(err) {
-            return res.json(err);
-        });
-})
+app.use('/', require('./lib/status'));
+app.use('/api/stations', require('./lib/stations'));
+app.use(errorHandler);
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));

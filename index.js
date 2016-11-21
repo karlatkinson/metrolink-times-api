878ba33e6d17fecdb0fd1e54dbeed96eac9fff94
Karl Atkinson <karl.atkinson@gmail.com>
2016-11-21 10:53:23 +0000

    make it work betters

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const port = process.env.PORT || 5000;
 
 app.use('/', require('./lib/status'));
 app.use('/find/trams', require('./lib/tramFinder'));
-app.use('/api/stations', require('./lib/tramTimes'));
+app.use('/api/stations', require('./lib/tramTimes').router);
 app.use(errorHandler);
 
 stations.getStations('tram').then(tramStations => {
     app.set('stations', tramStations);
+    app.set('port', port);
 
     app.listen(port, () => {
         console.log('Node app is running on port', port);

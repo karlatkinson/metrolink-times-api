80b37442024f8a6b98edba0db45472c44aae6d44
Karl Atkinson <karl.atkinson@gmail.com>
2016-11-21 10:07:37 +0000

    something like getting trams by destination

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const errorHandler = require('./lib/errorHandler');
+const stations = require('./lib/stations');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/', require('./lib/status'));
-app.use('/api/stations', require('./lib/stations'));
+app.use('/find/trams', require('./lib/tramFinder'));
+app.use('/api/stations', require('./lib/tramTimes'));
 app.use(errorHandler);
 
-app.listen(port, () => {
-    console.log('Node app is running on port', port);
+stations.getStations('tram').then(tramStations => {
+    app.set('stations', tramStations);
+
+    app.listen(port, () => {
+        console.log('Node app is running on port', port);
+    });
 });
 
 exports = module.exports = app;
